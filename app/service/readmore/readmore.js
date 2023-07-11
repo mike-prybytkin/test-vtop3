928f3686d80505df4e75d474acde15014d87d8be
@Mike-Prybytkin <prybytkin@bk.ru>
2023-07-12 00:41:01 +0300

    feat: implement readmore feature

---
+++ app/service/readmore/readmore.js
@@ -0,0 +1,19 @@
+const expandBlock = (e) => {
+	const button = e.target;
+	button.parentNode.classList.toggle("readmore-expand");
+};
+
+const readMoreText = () => {
+	const readmoreElements = document.querySelectorAll(".readmore");
+
+	readmoreElements.forEach((el) => {
+		if (el.scrollWidth > el.offsetWidth) {
+			const button = document.createElement("button");
+			button.classList.add("read-more__button", "button");
+			button.addEventListener("click", (e) => expandBlock(e));
+			el.appendChild(button);
+		}
+	});
+};
+
+readMoreText();
